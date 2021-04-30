61439427f9f3769136529fd53de27bca4d679fe9
Sergey <ssm1988@inbox.ru>
2021-04-30 12:11:31 +1000

    Добавьте файлы проекта.

---
+++ Module8FilesTask2/FileSize.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module8Files
+{
+    public class FileSize
+    {
+        public long FileSizeCount(string folder)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(folder);
+            DirectoryInfo[] directories = dirInfo.GetDirectories();
+            FileInfo[] FI = dirInfo.GetFiles();
+            long allFileSize = 0;
+            
+            if (dirInfo.Exists)
+            {
+                foreach (FileInfo file in FI)
+                {
+                    allFileSize += file.Length;                 
+                }
+                foreach (DirectoryInfo DI in directories)
+                {
+                    allFileSize += FileSizeCount(DI.FullName);
+                }
+            }
+             return allFileSize;
+        }
+    }
+}
---
+++ Module8FilesTask2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace Module8Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileSize test = new FileSize();
+
+            Console.WriteLine($"Общий размр папки: {test.FileSizeCount("C:/Users/Анатолий/Desktop/testFolder")}");
+        }
+    }
+}
+
